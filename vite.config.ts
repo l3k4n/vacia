db28811460a76a8f39f269a092df8f88e49158d1
lekan <alowoojalekan@gmail.com>
2023-05-27 13:41:13 -0500

    setup import aliases

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,14 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import eslint from "vite-plugin-eslint";
 import svgr from "vite-plugin-svgr";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   plugins: [react(), eslint(), svgr()],
 });
