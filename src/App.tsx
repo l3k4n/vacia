cdd765f69dc1cf0ae8369704720bd0a963b9092b
Lekan <codercruize@gmail.com>
2023-05-27 21:06:09 -0500

    Add: toolbars + created toolbars and utilitybars with icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,96 @@
+import React, { useState } from "react";
+import {
+  EllipseIcon,
+  FullScreenIcon,
+  HandIcon,
+  PenIcon,
+  RectIcon,
+  SelectIcon,
+  ZoomInIcon,
+  ZoomOutIcon,
+} from "@/assets/icons";
+import ToolBar, { UtilityBar, ToolBarDivider } from "@/components/ToolBar";
+import ToolButton from "@/components/ToolButton";
+import {
+  TOOLBAR_POSITIONS,
+  DESIGN_TOOLS,
+  UTILITY_TOOLS,
+} from "@/utils/constants";
+import "@/css/App.scss";
+// TODO: create Draggable ToolMenu component
+
 function App() {
-  return <div className="App" />;
+  const [activeDesignTool, setActiveDesignTool] = useState<DESIGN_TOOLS>(
+    DESIGN_TOOLS.HAND,
+  );
+  const DesignToolItem = (
+    props: React.PropsWithChildren<{
+      label: DESIGN_TOOLS;
+      onChange?: () => void;
+    }>,
+  ) => (
+    <ToolButton
+      {...props}
+      type="radio"
+      name="selected-tool"
+      label={`${props.label} tool`}
+      checked={props.label === activeDesignTool}
+      onChange={() => {
+        setActiveDesignTool(props.label);
+        props.onChange?.();
+      }}
+    />
+  );
+  const ButtonToolItem = (
+    props: React.PropsWithChildren<{
+      label: UTILITY_TOOLS;
+      onClick?: () => void;
+    }>,
+  ) => (
+    <ToolButton
+      {...props}
+      type="button"
+      label={props.label}
+      onClick={() => {
+        props.onClick?.();
+      }}
+    />
+  );
+  return (
+    <div className="app">
+      <div className="tools">
+        <ToolBar options={{ position: TOOLBAR_POSITIONS.LEFT }}>
+          <DesignToolItem label={DESIGN_TOOLS.HAND} children={<HandIcon />} />
+          <ToolBarDivider />
+          <DesignToolItem
+            label={DESIGN_TOOLS.SELECT}
+            children={<SelectIcon />}
+          />
+          <DesignToolItem label={DESIGN_TOOLS.RECT} children={<RectIcon />} />
+          <DesignToolItem
+            label={DESIGN_TOOLS.ELLIPSE}
+            children={<EllipseIcon />}
+          />
+          <DesignToolItem label={DESIGN_TOOLS.PEN} children={<PenIcon />} />
+        </ToolBar>
+        <UtilityBar>
+          <ButtonToolItem
+            children={<ZoomOutIcon />}
+            label={UTILITY_TOOLS.ZOOM_OUT}
+          />
+          <ButtonToolItem
+            children={<ZoomInIcon />}
+            label={UTILITY_TOOLS.ZOOM_IN}
+          />
+          <ToolBarDivider />
+          <ButtonToolItem
+            children={<FullScreenIcon />}
+            label={UTILITY_TOOLS.FULLSCREEN}
+          />
+        </UtilityBar>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/utils/constants.ts
@@ -0,0 +1,18 @@
+export const enum TOOLBAR_POSITIONS {
+  TOP = "ToolBar_position_top",
+  LEFT = "ToolBar_position_left",
+  RIGHT = "ToolBar_position_right",
+  BOTTOM = "ToolBar_position_bottom",
+}
+export const enum DESIGN_TOOLS {
+  HAND = "Hand",
+  SELECT = "Selection",
+  RECT = "Rectangle",
+  ELLIPSE = "Ellipse",
+  PEN = "pen",
+}
+export const enum UTILITY_TOOLS {
+  ZOOM_IN = "Zoom In",
+  ZOOM_OUT = "Zoom Out",
+  FULLSCREEN = "d",
+}
