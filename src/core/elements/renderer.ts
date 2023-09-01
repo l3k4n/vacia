128fba473ae034b78134142db1235a80927b3bc2
lekan <alowoojalekan@gmail.com>
2023-09-01 01:41:49 -0500

    Fix: invert ellipse when radius is negative

--- src/core/elements/renderer.ts
+++ src/core/elements/renderer.ts
@@ -7,10 +7,20 @@ function renderShapeElement(ctx: CanvasRenderingContext2D, elem: ShapeElement) {
   if (elem.shape === "rect") {
     ctx.fillRect(elem.x, elem.y, elem.w, elem.h);
   } else {
-    const rX = elem.w / 2;
-    const rY = elem.h / 2;
-    const cX = elem.x + rX;
-    const cY = elem.y + rY;
+    let rX = elem.w / 2;
+    let rY = elem.h / 2;
+    let cX = elem.x + rX;
+    let cY = elem.y + rY;
+
+    // invert ellipse if radius is negative
+    if (rX < 0) {
+      rX = Math.abs(rX);
+      cX = elem.x - rX;
+    }
+    if (rY < 0) {
+      rY = Math.abs(rY);
+      cY = elem.y - rY;
+    }
 
     ctx.beginPath();
     ctx.ellipse(cX, cY, rX, rY, 0, 0, 2 * Math.PI, false);
